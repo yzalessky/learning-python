3f4c83255797fd5ea2ef2fbbde8eb966cb399b1c
yzalessky <yzalessky@gmail.com>
2026-05-15 17:57:26 +0300

    homework 1 - fizzbuzz

---
+++ homework1.py
@@ -0,0 +1,9 @@
+for i in range(1, 51):
+  if i % 3 == 0 and i % 5 ==0:
+    print(f"{i} fizzbuzz")
+  elif i % 3 == 0:
+    print(f"{i} fizz")
+  elif i % 5 == 0:
+    print(f"{i} buzz")
+  else:
+    print(i)
