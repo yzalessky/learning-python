76921e4fa306699eae170ef9152decc8c868f06e
yzalessky <yzalessky@gmail.com>
2026-05-21 19:16:39 +0300

    homework 7 - caesar cipher

---
+++ homework7.py
@@ -0,0 +1,15 @@
+def caesar_cipher (text, shift):
+    base_text = list(text)
+    encrypted_text = []
+    for i in base_text:
+        if i.isupper():
+            e = chr((ord(i) - 65 + shift) % 26 + 65)
+        elif i.isalpha():
+            e = chr((ord(i) - 97 + shift) % 26 + 97)
+        else:
+            e = i
+        encrypted_text.append(e)
+    return("".join(encrypted_text))
+
+
+print(caesar_cipher("Hello World!", 3))
